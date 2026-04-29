e3510c0659c7adb23b165606d2dcf6091192bea7
matteveronese <matteoveronese04@gmail.com>
2026-04-29 18:27:07 +0200

    Creazione del progetto utilizzando l’archetipo maven quick start

---
+++ src/main/java/com/mycompany/app/IntegerToRoman.java
@@ -0,0 +1,11 @@
+// prova pratica
+
+package com.mycompany.app;
+
+public class IntegerToRoman {
+  public static String convert(int number){
+    // TODO
+    return null;
+  }
+}
+
---
+++ src/main/java/com/mycompany/app/RomanPrinter.java
@@ -0,0 +1,14 @@
+// prova pratica
+
+package com.mycompany.app;
+
+public class RomanPrinter {
+  public static String print(int num){
+    return printAsciiArt(IntegerToRoman.convert(num));
+  }
+  private static String printAsciiArt(String romanNumber){
+    //TODO
+    return null;
+  }
+}
+
