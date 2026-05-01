321d130c8dc848c757db8391bb2b715970e0f551
matteveronese <matteoveronese04@gmail.com>
2026-05-01 10:41:43 +0200

    check convert with 1-1000 by going strToInt and compare it to the initial int value

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -18,6 +18,33 @@ public class AppTest
         assertTrue( true );
     }
 
+    static int[] values = {1000, 500, 100, 50, 10, 5, 1};
+    static char[] symbols = {'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+    static char[] subractives = {'C', 'X', 'I'};
+    static int[] subractivesValues = {100, 10, 1};
+
+    private void check(String str, int num) {
+        int r = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            int j = 0;
+            for (; j < symbols.length; j++)
+                if (symbols[j] == c) {
+                    for (int k = 0; k < subractives.length; k++)
+                        if (i > 0 && str.charAt(i - 1) == subractives[k] && values[j] - subractivesValues[k] > 0) {
+                            r -= subractivesValues[k] * 2; // prima lo ho aggiunto senza guardare dopo...
+                            break;
+                        }
+
+                    r += values[j];
+                    break;
+                }
+            if (j == symbols.length)
+                assertTrue("Invalid symbol: " + c, false);
+        }
+        assertTrue(r == num);
+    }
+
     @Test
     public void convert()
     {
@@ -31,12 +58,16 @@ public class AppTest
         assertTrue("M".compareTo(IntegerToRoman.convert(1000)) == 0);
         
         assertTrue("III".compareTo(IntegerToRoman.convert(3)) == 0);
+        check("III", 3);
         assertTrue("CCLIX".compareTo(IntegerToRoman.convert(259)) == 0);
+        check("CCLIX", 259);
         assertTrue("XCII".compareTo(IntegerToRoman.convert(92)) == 0);
         assertTrue("LVIII".compareTo(IntegerToRoman.convert(58)) == 0);
         assertTrue("CMXCIV".compareTo(IntegerToRoman.convert(994)) == 0);
         assertTrue("MDCLXVI".compareTo(IntegerToRoman.convert(1666)) == 0);
-        
+
+        for (int i = 1; i < 1000; i++)
+            check(IntegerToRoman.convert(i), i);
     }
 
     @Test
