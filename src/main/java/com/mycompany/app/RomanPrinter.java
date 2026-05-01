2ac1e4d9020b61cb672b5b71ea678100a17b12d6
matteveronese <matteoveronese04@gmail.com>
2026-05-01 11:07:23 +0200

    printAsciiArt implementation

--- src/main/java/com/mycompany/app/RomanPrinter.java
+++ src/main/java/com/mycompany/app/RomanPrinter.java
@@ -6,9 +6,30 @@ public class RomanPrinter {
   public static String print(int num){
     return printAsciiArt(IntegerToRoman.convert(num));
   }
+
+  static char[] symbols = {'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+  static String[] asciiArt = {
+            "  __  __  ",            " |  \\/  | ",            " | \\  / | ",            " | |\\/| | ",            " | |  | | ",            " |_|  |_| ",            "  _____   ",            " |  __ \\  ",            " | |  | | ",            " | |  | | ",            " | |__| | ",            " |_____/  ",            "   _____  ",            "  / ____| ",            " | |      ",            " | |      ",            " | |____  ",            "  \\_____| ",            "  _       ",            " | |      ",            " | |      ",            " | |      ",            " | |____  ",            " |______| ",            " __    __ ",            " \\ \\  / / ",            "  \\ \\/ /  ",            "   >  <   ",            "  / /\\ \\  ",            " /_/  \\_\\ ",            "__      __",            "\\ \\    / /",            " \\ \\  / / ",            "  \\ \\/ /  ",            "   \\  /   ",            "    \\/    ",            "  ______  ",            " |_    _| ",            "   |  |   ",            "   |  |   ",            "  _|  |_  ",            " |______| "
+    
+  };
+  static int line_high = 6;
+
   private static String printAsciiArt(String romanNumber){
-    // TODO
-    return null;
+    String result = "";
+    for (int i = 0; i < line_high; i++) {
+      for (char c : romanNumber.toCharArray()) {
+        int index = -1;
+        for (int j = 0; j < symbols.length; j++)
+          if (symbols[j] == c) {
+            index = j;
+            break;
+          }
+        if (index != -1)
+          result += asciiArt[index * line_high + i];
+      }
+      result += "\n";
+    }
+    return result;
   }
 }
 
